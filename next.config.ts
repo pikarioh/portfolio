ef1d5cb56d13e2584f7c59726aff7c24d6fed7a1
pikarioh <thanathonb@icloud.com>
2025-07-07 23:04:40 +0200

    Configured assets basepath

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production"
+
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: isProduction ? '/portfolio' : '',
   output: 'export',
   images: {
     unoptimized: true,
